e121a2219ef12e3e02c041c1cee9b56366d14bc1
gpn39f <frankberg@foni.net>
2014-01-25 21:12:52 +0100

    mehr deutsche texte

---
+++ updates/2014_01_21_01_deutsch.sql
@@ -0,0 +1,29 @@
+
+-- quest 26799
+UPDATE locales_quest SET QuestGiverTextWindow_loc3="Hirnloser Zombie",QuestGiverTargetName_loc3="Hirnloser Zombie" WHERE Id=26799;
+
+-- npc 49230 gossip text 17566
+update locales_npc_text set Text0_0_loc3="Tretet zurück, Monster. Ihr wollt einen Kampf? Ich werde gegen Euch kämfen.$B$BIch werde gegen jeden von euch Kreaturen kämfen! Hören Sie mich?" where ID=17566;
+
+-- npc 49230 gossip text 17567
+update locales_npc_text set Text0_0_loc3="Ach, wirklich?$B$BPrima. Nein, ich werde nicht Ihren verlassenen beizutreten. Vielleicht werde ich meine eigene Verlassenen gründen! Vielleicht werde ich Verlassene mit Ellbogen zu erfinden!" where ID=17567;
+
+-- npc 49231 gossip text 17569
+update locales_npc_text set Text0_0_loc3="Was ... was ist los? Wer seid Ihr? Was ist mit mir passiert?" where ID=17569;
+
+-- npc 49231 gossip text 17570
+update locales_npc_text set Text0_0_loc3="I. .. starb? Ja, Sie haben Recht. Ich starb. Es war ein Ork ... Er schnitt mir meine Hände ab und ließ mich zum sterben liegen. <Valdred schaut auf seine Hände nach unten.> Das sind nicht meine Hände! DAS SIND NICHT MEINE HÄNDE!" where ID=17570;
+
+-- quest 26800
+UPDATE locales_quest SET QuestGiverTextWindow_loc3="Leiche der Scharlachroten ",QuestGiverTargetName_loc3="Leiche der Scharlachroten " WHERE Id=26800;
+
+
+
+
+
+
+
+-- npc 49230 gossip text 17567
+-- update locales_npc_text set Text0_0_loc3="" where ID=17567;
+
+
